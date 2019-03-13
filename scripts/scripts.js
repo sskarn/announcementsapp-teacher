c34cdf4268880f68a2bbc4db493bf8617108e205
Karnbir S <karnbirsaini@hotmail.com>
2019-03-13 16:29:58 -0400

    adding existing files
    scripts.js (variables: title, grade, gender, club, details - getData function -> pull elementbyid from html(details) -> post alert)\
    
    creator.html (JS, title, details, grade, gender, club, submit button with getData function ref)

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+var annTitle;
+var annGrade;
+var annGender;
+var annClubs;
+var annDetails;
+
+function getData() {
+annDetails = document.getElementById("details").value;
+alert(annDetails);
+}
+
+
