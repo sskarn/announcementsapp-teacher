7c0d6ac5bf1c3901ef1e51d4a086d572bbdfb43f
Karnbir S <karnbirsaini@hotmail.com>
2019-03-19 13:08:43 -0400

    Adding Local Storage
    Keys Created: Title, Details, Grade, Gender, Club

--- scripts/creatorscript.js
+++ scripts/creatorscript.js
@@ -11,6 +11,11 @@ annGrade = document.getElementById("grade").value;
 annGender = document.getElementById("gender").value;
 annClubs = document.getElementById("clubs").value;
 alert(annTitle + annDetails + annGrade + annGender + annClubs);
+localStorage.setItem("Title:", annTitle);
+localStorage.setItem("Details:", annDetails);
+localStorage.setItem("Grade:", annGrade);
+localStorage.setItem("Gender:", annGender);
+localStorage.setItem("Club:", annClubs);
 }
 
 
