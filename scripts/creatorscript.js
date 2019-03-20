4899c916f624faf9a5910f72ab78fb13aa8c907d
Karnbir S <karnbirsaini@hotmail.com>
2019-03-20 13:40:17 -0400

    Rename text/variables from clubs to club

--- scripts/creatorscript.js
+++ scripts/creatorscript.js
@@ -2,20 +2,20 @@ var annTitle;
 var annDetails;
 var annGrade;
 var annGender;
-var annClubs;
+var annClub;
 
 function getData() {
 annTitle = document.getElementById("title").value;
 annDetails = document.getElementById("details").value;
 annGrade = document.getElementById("grade").value;
 annGender = document.getElementById("gender").value;
-annClubs = document.getElementById("clubs").value;
-alert(annTitle + annDetails + annGrade + annGender + annClubs);
+annClub = document.getElementById("club").value;
+alert(annTitle + annDetails + annGrade + annGender + annClub);
 localStorage.setItem("Title:", annTitle);
 localStorage.setItem("Details:", annDetails);
 localStorage.setItem("Grade:", annGrade);
 localStorage.setItem("Gender:", annGender);
-localStorage.setItem("Club:", annClubs);
+localStorage.setItem("Club:", annClub);
 }
 
 
