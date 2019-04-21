bb0610e84950ce235bb4ca7f0bdd519f492a3ba9
Karnbir Saini <karnbirsaini@hotmail.com>
2019-04-20 23:33:10 -0400

    Splitting up one huge function into multiple for cleanliness
    Cleanliness issue with prior implementation was that all of the code was in one huge function. As a result, I decided to split it up into 3 seperate functions with a function to run them all accordingly. I noticed that the code in the else statement was identical to the code inside the end of the if statement so functions would make it simpler.
    
    Function getAnnInfo: responsible for retrieving the announcement info from the html to put them into arrays and adding a timestamp to the announcement from System Time
    
    Function storeAnn: stores the current index as the AnnCount key, and puts all the announcement info from the arrays into respective Local Storage keys
    
    Function retrieveAnn: retrieves the Local Storage keys for the announcements to put them into arrays for the JS (to prevent null error once page is refreshed due to no prior announcements being found in the JS after refresh)
    
    Function createAnn: run once the teacher clicks the submit button
    Initially (with no announcements in local storage and an index of 0) the else statement will run the getAnnInfo function and storeAnn function
    After (once there is at least one announcement in local storage and an index value in AnnCount)
    1. set the active JS index to the value from AnnCount
    2. utilize the retrieveAnn function to populate arrays
    3. increment index by 1 to allow new announcements to be created with a higher index
    4. run getAnnInfo and storeAnn
    
    Voila! Simplifed and easy to understand code, even for the novice coder.

--- scripts/creatorscript.js
+++ scripts/creatorscript.js
@@ -6,45 +6,45 @@ var annClub = [];
 var annDateTime = [];
 var index = 0;
 
-function getData() {
+function getAnnInfo() {
+    annTitle[index] = document.getElementById("title").value;
+    annDetails[index] = document.getElementById("details").value;
+    annGrade[index] = document.getElementById("grade").value;
+    annGender[index] = document.getElementById("gender").value;
+    annClub[index] = document.getElementById("club").value;
+    annDateTime[index] = new Date().toLocaleString();
+}
+
+function storeAnn() {
+    localStorage.setItem("AnnCount:", index);
+    localStorage.setItem("Title:", JSON.stringify(annTitle));
+    localStorage.setItem("Details:", JSON.stringify(annDetails));
+    localStorage.setItem("Grade:", JSON.stringify(annGrade));
+    localStorage.setItem("Gender:", JSON.stringify(annGender));
+    localStorage.setItem("Club:", JSON.stringify(annClub));
+    localStorage.setItem("DateTime:", JSON.stringify(annDateTime));
+}
+
+function retrieveAnn() {
+    annTitle = JSON.parse(localStorage.getItem("Title:"));
+    annDetails = JSON.parse(localStorage.getItem("Details:"));
+    annGrade = JSON.parse(localStorage.getItem("Grade:"));
+    annGender = JSON.parse(localStorage.getItem("Gender:"));
+    annClub = JSON.parse(localStorage.getItem("Club:"));
+    annDateTime = JSON.parse(localStorage.getItem("DateTime:"));
+}
+
+function createAnn() {
     if (localStorage.getItem("AnnCount:")) {
         index = JSON.parse(localStorage.getItem("AnnCount:"));
-        annTitle = JSON.parse(localStorage.getItem("Title:"));
-        annDetails = JSON.parse(localStorage.getItem("Details:"));
-        annGrade = JSON.parse(localStorage.getItem("Grade:"));
-        annGender = JSON.parse(localStorage.getItem("Gender:"));
-        annClub = JSON.parse(localStorage.getItem("Club:"));
-        annDateTime = JSON.parse(localStorage.getItem("DateTime:"));
+        retrieveAnn();
         index += 1;
-        annTitle[index] = document.getElementById("title").value;
-        annDetails[index] = document.getElementById("details").value;
-        annGrade[index] = document.getElementById("grade").value;
-        annGender[index] = document.getElementById("gender").value;
-        annClub[index] = document.getElementById("club").value;
-        annDateTime[index] = new Date().toLocaleString();
-        localStorage.setItem("AnnCount:", index);
-        localStorage.setItem("Title:", JSON.stringify(annTitle));
-        localStorage.setItem("Details:", JSON.stringify(annDetails));
-        localStorage.setItem("Grade:", JSON.stringify(annGrade));
-        localStorage.setItem("Gender:", JSON.stringify(annGender));
-        localStorage.setItem("Club:", JSON.stringify(annClub));
-        localStorage.setItem("DateTime:", JSON.stringify(annDateTime));
+        getAnnInfo();
+        storeAnn();
     } else {
-        annTitle[index] = document.getElementById("title").value;
-        annDetails[index] = document.getElementById("details").value;
-        annGrade[index] = document.getElementById("grade").value;
-        annGender[index] = document.getElementById("gender").value;
-        annClub[index] = document.getElementById("club").value;
-        annDateTime[index] = new Date().toLocaleString();
-        localStorage.setItem("AnnCount:", index);
-        localStorage.setItem("Title:", JSON.stringify(annTitle));
-        localStorage.setItem("Details:", JSON.stringify(annDetails));
-        localStorage.setItem("Grade:", JSON.stringify(annGrade));
-        localStorage.setItem("Gender:", JSON.stringify(annGender));
-        localStorage.setItem("Club:", JSON.stringify(annClub));
-        localStorage.setItem("DateTime:", JSON.stringify(annDateTime));
+        getAnnInfo();
+        storeAnn();
     }
-	
 }
 
 
