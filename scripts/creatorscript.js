47e4a9c7889cf89abdc251161105999d5b87dab3
Karnbir Saini <karnbirsaini@hotmail.com>
2019-04-20 23:15:05 -0400

    Fixed MAJOR BUG for Creating Announcements (Announcements being overwritten on refresh)
    Bug: Old implementation of the teacher creator (removed code), due to the index var being set to 0 every time the page was refreshed (due to the declaration of the index var), old announcements were being overwritten due to the new announcement after refresh having the same index of 0, 1, and so on
    
    Process of Solving (Using Online Resources):
    https://www.taniarascia.com/how-to-use-local-storage-with-javascript/ - in the "Integrating Local Storage" section, it is explained that a conditional statement must be created in order to check for an existing local storage key (which in this case is the 'AnnCount:' that I created in order for it to match with the index var) to keep the information on refresh
    
    https://stackoverflow.com/questions/22726080/how-to-retain-javascript-array-while-page-refresh - CodeMaster explains that the array must be retrieved from local storage in order for it to be retained on refresh
    
    How I Solved the Issue
    - Created an if statement with a else statement linked to it
    - the else statement is only utilized when the AnnCount key doesnt exist (meaning that there is no prior announcements stored in local storage, index = 0)
    - that else statement works similarly to the removed code, pulling all of the info from the html, and setting it to local storage BUT another key is set called AnnCount which is equal to the index of 0
    - after an announcement is created and set into local storage, the if statement is then activated which tests for the existence of the AnnCount key (this means there is an announcement in local storage) and it runs its code as per the following:
    1. setting index var to the value of the AnnCount key (as AnnCount keeps a running track of indexes after refreshes unlike the former implementation)
    2. Getting the existing announcements in local storage and putting them into their respective arrays (if this is not done, after refresh, the code will not find the prior announcements [which are only in Local Storage] and will set all prior values to null, only keeping the announcements created after refresh, which is the exact problem as before) - This was the hardest part to solve, as I didn't think that the arrays had to be set again
    3. index count is incremented up by 1, allowing new announcements to be stored
    4. same function as else statement (pulling info from html + timestamp, setting to local storage)
    
    This was my biggest struggle yet and is my big "WOW" moment from JavaScript (As far as I know, no one has solved this problem with their code)
    
    If this code could be cleaned up more, it would be a lot more professional and easier to understand.

--- scripts/creatorscript.js
+++ scripts/creatorscript.js
@@ -7,19 +7,44 @@ var annDateTime = [];
 var index = 0;
 
 function getData() {
-	annTitle[index] = document.getElementById("title").value;
-	annDetails[index] = document.getElementById("details").value;
-	annGrade[index] = document.getElementById("grade").value;
-	annGender[index] = document.getElementById("gender").value;
-	annClub[index] = document.getElementById("club").value;
-    annDateTime[index] = new Date().toLocaleString();
-	index += 1;
-	localStorage.setItem("Title:", JSON.stringify(annTitle));
-	localStorage.setItem("Details:", JSON.stringify(annDetails));
-	localStorage.setItem("Grade:", JSON.stringify(annGrade));
-	localStorage.setItem("Gender:", JSON.stringify(annGender));
-	localStorage.setItem("Club:", JSON.stringify(annClub));
-    localStorage.setItem("DateTime:", JSON.stringify(annDateTime));
+    if (localStorage.getItem("AnnCount:")) {
+        index = JSON.parse(localStorage.getItem("AnnCount:"));
+        annTitle = JSON.parse(localStorage.getItem("Title:"));
+        annDetails = JSON.parse(localStorage.getItem("Details:"));
+        annGrade = JSON.parse(localStorage.getItem("Grade:"));
+        annGender = JSON.parse(localStorage.getItem("Gender:"));
+        annClub = JSON.parse(localStorage.getItem("Club:"));
+        annDateTime = JSON.parse(localStorage.getItem("DateTime:"));
+        index += 1;
+        annTitle[index] = document.getElementById("title").value;
+        annDetails[index] = document.getElementById("details").value;
+        annGrade[index] = document.getElementById("grade").value;
+        annGender[index] = document.getElementById("gender").value;
+        annClub[index] = document.getElementById("club").value;
+        annDateTime[index] = new Date().toLocaleString();
+        localStorage.setItem("AnnCount:", index);
+        localStorage.setItem("Title:", JSON.stringify(annTitle));
+        localStorage.setItem("Details:", JSON.stringify(annDetails));
+        localStorage.setItem("Grade:", JSON.stringify(annGrade));
+        localStorage.setItem("Gender:", JSON.stringify(annGender));
+        localStorage.setItem("Club:", JSON.stringify(annClub));
+        localStorage.setItem("DateTime:", JSON.stringify(annDateTime));
+    } else {
+        annTitle[index] = document.getElementById("title").value;
+        annDetails[index] = document.getElementById("details").value;
+        annGrade[index] = document.getElementById("grade").value;
+        annGender[index] = document.getElementById("gender").value;
+        annClub[index] = document.getElementById("club").value;
+        annDateTime[index] = new Date().toLocaleString();
+        localStorage.setItem("AnnCount:", index);
+        localStorage.setItem("Title:", JSON.stringify(annTitle));
+        localStorage.setItem("Details:", JSON.stringify(annDetails));
+        localStorage.setItem("Grade:", JSON.stringify(annGrade));
+        localStorage.setItem("Gender:", JSON.stringify(annGender));
+        localStorage.setItem("Club:", JSON.stringify(annClub));
+        localStorage.setItem("DateTime:", JSON.stringify(annDateTime));
+    }
+	
 }
 
 
